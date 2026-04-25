649324b69260809de61551b4836a04d34ecaf4fb
Ale007XD <alex.deloverov@gmail.com>
2026-04-25 20:58:42 +0700

    Suppress lint warning for LiteLLMAdapter import
    Add noqa comment to suppress linting error for unused import.

--- nano_vm/adapters/__init__.py
+++ nano_vm/adapters/__init__.py
@@ -3,7 +3,7 @@ from .base import LLMAdapter
 __all__ = ["LLMAdapter"]
 
 try:
-    from .litellm_adapter import LiteLLMAdapter
+    from .litellm_adapter import LiteLLMAdapter  # noqa: F401
 
     __all__.append("LiteLLMAdapter")
 except ImportError:
